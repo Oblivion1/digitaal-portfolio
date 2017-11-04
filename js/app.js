e3a3de3a0b1001a3d83a2ae8134dc3c4ea83bd08
Oblivion <99048742@mydavinci.nl>
2017-11-04 10:07:29 +0100

    no message

---
+++ js/app.js
@@ -0,0 +1,74 @@
+
+particlesJS('particles-js',
+
+  {
+    "particles": {
+      "number": {
+        "value": 30,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+      },
+      "opacity": {
+        "value": 0.4,
+        "random": false,
+        "anim": {
+          "enable": true,
+          "speed": 0.1,
+          "opacity_min": 0,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+      },
+      "move": {
+        "enable": true,
+        "speed": 1,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": false,
+      },
+    },
+    "retina_detect": false,
+  }
+
+);
